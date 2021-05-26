141c3c6caa365cf2e4ec86b4012081828ceccfeb
arnaldo.quezada.laptopTrabajo <arnaldo.quezada@gmail.com>
2021-05-26 16:20:01 -0400

    Sin errores listo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import React,{ useState } from 'react';
+import React from 'react';
 import './App.scss';
 import UserForm from './components/UserForm'
 
 function App() {
-  const [state, setState] = useState(
-    {
-      firstName:"",
-      lastName:"",
-      email:"",
-      password:"",
-      confirmPassword:""
-    });
+ 
   return (
     <div className="App">
-      <UserForm inputs = {state}/>
-      {/* < DataForm/> */}
+      <UserForm/>
+   
     </div>
   );
 }
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,28 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react'
 
-const UserForm = (props) => {    
+const UserForm = (props) => {  
+    const [state, setState] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+    
+
+    const onChangeHandler = e => {
+            setState({
+                ...state,
+                [e.target.name]: e.target.value,
+            });
+        }
+        console.log(state)
     return(
         <form action="">
             <form-container>
                 <label htmlFor="firstName">First Name</label>
-                <input type="text" name="firstName"/>
+                <input onChange={onChangeHandler} type="text" name="firstName"/>
             </form-container>
             <form-container>
                 <label htmlFor="lastName">Last Name</label>
-                <input type="text" name="firstName"/>
+                <input onChange={onChangeHandler} type="text" name="lastName"/>
             </form-container>
             <form-container>
                 <label htmlFor="email">Email</label>
-                <input type="text" name="email"/>
+                <input onChange={onChangeHandler} type="text" name="email"/>
             </form-container>
             <form-container>                 
-                <label htmlFor="firstName">Password</label>
-                <input type="text" name="password"/>
+                <label htmlFor="password">Password</label>
+                <input onChange={onChangeHandler} type="password" name="password"/>
             </form-container>          
             <form-container>
-                <label htmlFor="firstName">Confirm Password</label>
-                <input type="text" name="confirmPassword"/>
-            </form-container>         
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input onChange={onChangeHandler} type="password" name="confirmPassword"/>
+            </form-container>    
+            <div className="error">
+                {(state.firstName.length !==0 && state.firstName.length <2) && <p>First Name must be 2 Characters long</p>}
+                {(state.lastName.length !== 0 && state.lastName.length < 2) && <p>Last Name must be 2 Characters long</p>}
+                {(state.email.length !== 0 && state.email.length < 5) && <p>Email must be 5 Characters long</p>}
+                {(state.password.length !== 0 && state.password.length < 8) && <p>Password must be 8 Characters long</p>}
+                {(state.confirmPassword !== state.password) && <p>Confirm Password must match Password</p>}
+            </div>     
         </form>
     )
 }
